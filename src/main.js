f7ee2cadc56b8d11345a00039b062ae0465f9d12
Devadutt-11 <anjukichus984@gmail.com>
2026-09-18 22:34:41 +0530

    in

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
 const API_KEY = import.meta.env.VITE_NASA_API_KEY;
-const date = document.querySelector("#datepicker").value;
-fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}`)
 document.querySelector("#app").innerHTML = "<p>Loading...</p>";
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
     .then((response) => response.json())
