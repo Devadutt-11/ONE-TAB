596da9e6c69890d40be9d2c37a5e546a1ab34ba7
Devadutt-11 <anjukichus984@gmail.com>
2026-09-19 22:57:10 +0530

    aaaaaa

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 const API_KEY = import.meta.env.VITE_NASA_API_KEY;
 document.querySelector("#app").innerHTML = "<p>Loading...</p>";
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
-    .then((response) => response.json())
-    .then(data => {
-        document .querySelector("#app").innerHTML = `
-        <h3>${data.title}</h3>
-        <img src="${data.url}" />
-        <p>${data.explanation}</p>
-        `;
-    })
+  .then((response) => response.json())
+  .then((data) => {
+    const media = data.media_type === "image"
+      ? `<img src="${data.url}" alt="${data.title}">`
+      : `<video src="${data.url}" controls></video>`;
+
+    document.querySelector("#app").innerHTML = `
+      <h3>${data.title}</h3>
+      ${media}
+      <p>${data.explanation}</p>
+    `;
+  });
